256f4cabcd0c90dbe9bb025ef3b340a369e1dd9d
Beast Code <beastcode9999@gmail.com>
2024-04-10 21:16:47 +0930

    add button id's

--- script.js
+++ script.js
@@ -6,44 +6,40 @@ function getElemById(id) {
 
 const Slider = (function Slider() {
     const _sliderButtons = document.querySelectorAll(".slider-button");
-    const _cards = document.querySelector(".cards--trending");
-    const _scrollBar = document.querySelector()
-
+    const _cards = getElemById("cards--trending");
+    const _scrollBar = getElemById("scrollbar");
+    const _maxScrollLeft = _cards.scrollWidth - _cards.clientWidth;
 
     const _scrollLeftRight = function _scrollLeftRight(twoButtons) {
         twoButtons.forEach( button =>  {
             button.addEventListener('click',() => {
                 const direction = button.id === "button-next" ? 1 : -1; // if button--next direction = 1 else -1
-                const scrollAmount = _cards.clientWidth * direction // if direction = 1, clientWidth is positive else negative 
+                const scrollAmount = _cards.clientWidth * direction; // if direction = 1, clientWidth is positive else negative 
                 // using scrollBy() method to add scroll functionaility
                 _cards.scrollBy({
                     left: scrollAmount,
-                    behavior: "smooth"
-                })
+                    behavior: "smooth",
+                })  
             })
         })
     } 
 
-    // const _maxMinButtonPopup = function _maxMinButtonPopup() {
-    //     const prevButton = document.querySelector('.slider-button--previous');
-    //     const nextButton = document.querySelector('.slider-button--next');
-    //     const maxScrollLeft = _cards.scrollWidth - _cards.clientWidth;
-
-    //     prevButton.style.display = _cards.scrollLeft <= 0 ? "none" : "block"
-    //     nextButton.style.display = _cards.scrollLeft >= maxScrollLeft ? "none" : "block"
-    // }
-
     const _scrollThumbPosition = function _scrollThumbPosition() {
         const scrollPosition = _cards.scrollLeft;
-        const maxScrollLeft = _cards.scrollWidth - _cards.clientWidth;
-        const thumbPosition = (scrollPosition/maxScrollLeft);
-
-        console.log(scrollPosition)
+        const scrollBarThumb = getElemById("scrollbar__thumb");
+        const thumbPosition = (scrollPosition/_maxScrollLeft) * (_scrollBar.clientWidth - scrollBarThumb.offsetWidth);
+        scrollBarThumb.style.left = `${thumbPosition}px`;
     }
 
+    
+
     const _updateSlider = function updateSlider() {
-        _scrollLeftRight(_sliderButtons)
-        _scrollThumbPosition()
+        _scrollLeftRight(_sliderButtons);
+       
+       _cards.addEventListener("scroll", ()=> {
+            _scrollThumbPosition();
+            _buttonDisplay();
+       })
         // _maxMinButtonPopup()
     };
 
