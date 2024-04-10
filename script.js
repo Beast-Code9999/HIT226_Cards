41ea195b5559c706616121b6389ecd0cac8c17ab
Beast Code <beastcode9999@gmail.com>
2024-04-10 20:52:14 +0930

    update _scrollThumbPosition()

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
 "use strict";
 
+function getElemById(id) {
+    return document.getElementById(id);
+}
+
+const Slider = (function Slider() {
+    const _sliderButtons = document.querySelectorAll(".slider-button");
+    const _cards = document.querySelector(".cards--trending");
+    const _scrollBar = document.querySelector()
 
 
+    const _scrollLeftRight = function _scrollLeftRight(twoButtons) {
+        twoButtons.forEach( button =>  {
+            button.addEventListener('click',() => {
+                const direction = button.id === "button-next" ? 1 : -1; // if button--next direction = 1 else -1
+                const scrollAmount = _cards.clientWidth * direction // if direction = 1, clientWidth is positive else negative 
+                // using scrollBy() method to add scroll functionaility
+                _cards.scrollBy({
+                    left: scrollAmount,
+                    behavior: "smooth"
+                })
+            })
+        })
+    } 
 
+    // const _maxMinButtonPopup = function _maxMinButtonPopup() {
+    //     const prevButton = document.querySelector('.slider-button--previous');
+    //     const nextButton = document.querySelector('.slider-button--next');
+    //     const maxScrollLeft = _cards.scrollWidth - _cards.clientWidth;
 
-window.addEventListener('click', ()=> {
+    //     prevButton.style.display = _cards.scrollLeft <= 0 ? "none" : "block"
+    //     nextButton.style.display = _cards.scrollLeft >= maxScrollLeft ? "none" : "block"
+    // }
 
-})
+    const _scrollThumbPosition = function _scrollThumbPosition() {
+        const scrollPosition = _cards.scrollLeft;
+        const maxScrollLeft = _cards.scrollWidth - _cards.clientWidth;
+        const thumbPosition = (scrollPosition/maxScrollLeft);
 
+        console.log(scrollPosition)
+    }
+
+    const _updateSlider = function updateSlider() {
+        _scrollLeftRight(_sliderButtons)
+        _scrollThumbPosition()
+        // _maxMinButtonPopup()
+    };
+
+    const init = function init() {
+        _updateSlider();
+     };
+ 
+     return {
+         init: init,
+     };
+})();
+
+// Turning off users ability to select and drag images or other elements if necessary
 const DraggableOff = (function DraggableOff() {
     let _images = document.getElementsByTagName('img');
 
@@ -15,7 +64,7 @@ const DraggableOff = (function DraggableOff() {
         for (let i = 0; i < _images.length; i++) {
             _images[i].setAttribute('draggable', false);
         }
-    }
+    };
 
     const init = function init() {
        _updateDraggableImg();
@@ -27,5 +76,6 @@ const DraggableOff = (function DraggableOff() {
 })();
 
 window.addEventListener('load', function() {
+    Slider.init();
     DraggableOff.init();
 });
