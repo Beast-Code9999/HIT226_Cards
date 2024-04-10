3701033f2466070115957d3782bd11c8d12562bc
Beast Code <beastcode9999@gmail.com>
2024-04-10 18:36:06 +0930

    add css to .section--recipes

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+
+
+
+
+window.addEventListener('click', ()=> {
+
+})
+
+const DraggableOff = (function DraggableOff() {
+    let _images = document.getElementsByTagName('img');
+
+    const _updateDraggableImg = function _updateDraggableImg() {
+        for (let i = 0; i < _images.length; i++) {
+            _images[i].setAttribute('draggable', false);
+        }
+    }
+
+    const init = function init() {
+       _updateDraggableImg();
+    };
+
+    return {
+        init: init,
+    };
+})();
+
+window.addEventListener('load', function() {
+    DraggableOff.init();
+});
